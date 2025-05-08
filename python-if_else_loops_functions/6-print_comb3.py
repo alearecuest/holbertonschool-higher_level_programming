d1211d2c52f620c14c7b79d596ef734e4a9e2a39
Alejandro <10653@holbertonstudents.com>
2025-05-08 16:20:34 -0300

    updating 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-for numbers in range(0, 9):
+for numbers in range(0, 10):
     for combinations in range(1, 10):
-        print("{}{}".format(numbers, combinations), end=", ")
+        if numbers >= combinations:
+            continue
+        elif numbers == 8 and combinations == 9:
+            print("{}{}".format(numbers, combinations))
+        else:
+            print("{}{}".format(numbers, combinations), end=", ")
