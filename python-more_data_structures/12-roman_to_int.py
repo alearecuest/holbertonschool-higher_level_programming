45ad34d571cb4ffddb3e7f3820386aec3c74cb83
Alejandro <10653@holbertonstudents.com>
2025-05-16 15:01:09 -0300

    Updating Pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    roman_libr = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_libr = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total_char = 0
     prev_value = 0
 
