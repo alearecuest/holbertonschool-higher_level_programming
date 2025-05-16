9a53f81a2a968cb42cdd2a9f9274962dccbf3cb6
Alejandro <10653@holbertonstudents.com>
2025-05-16 14:59:54 -0300

    Update checker

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
     roman_libr = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
     total_char = 0
     prev_value = 0
 
