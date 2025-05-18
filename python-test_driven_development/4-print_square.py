4f5a31ddf243a8d26994b44e8bac119b8bba8fec
Alejandro <10653@holbertonstudents.com>
2025-05-18 19:30:34 -0300

    More erros

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -18,7 +18,7 @@ def print_square(size):
         ValueError: If size is less than 0.
     """
     if not isinstance(size, int):
-        raise TypeError("size mut be an integer")
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     for n in range(size):
