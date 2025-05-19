3ee8804736b1fe536a44341ac20d8ce4e55cb2af
Alejandro <10653@holbertonstudents.com>
2025-05-19 12:24:57 -0300

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+The Square class is an empty class that defines a square.
+"""
+
+
+class Square:
+     """
+    This class represents a square.
+    The class is currently empty but will be expanded in future exercises to
+    include attributes and methods related to squares.
+    """
+     pass
