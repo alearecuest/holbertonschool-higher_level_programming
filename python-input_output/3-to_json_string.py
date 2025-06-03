2c50961c07d4513bc3278f9cc5da8316ce210caa
Alejandro <10653@holbertonstudents.com>
2025-06-03 15:38:01 -0300

    pycodestyle error

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -10,7 +10,7 @@ import json
 def to_json_string(my_obj):
     """
     Returns the JSON string representation of an object.
-    
+
     This function takes a Python object and converts it to
     a JSON formatted string using the json module.
 
@@ -22,7 +22,8 @@ def to_json_string(my_obj):
 
     Note:
         - Does not handle exceptions if the object can't be serialized
-        - Common serializable types include: dict, list, str, int, float, bool, None
+        - Common serializable types include: 
+            dict, list, str, int, float, bool, None
         - Non-serializable types (like sets) will raise TypeError
     """
     return json.dumps(my_obj)
