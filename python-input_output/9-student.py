dbba0a80252c647f1745c9e9d1aa0546f2bfcd4a
Alejandro <10653@holbertonstudents.com>
2025-06-05 17:28:33 -0300

    Updating Py Code Style

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,13 +7,13 @@ Module for Student class
 class Student:
     """
     Class that defines a student.
-    
+
     Attributes:
         first_name (str): The first name of the student
         last_name (str): The last name of the student
         age (int): The age of the student
     """
-    
+
     def __init__(self, first_name, last_name, age):
         """
         Initialize a new Student instance.
