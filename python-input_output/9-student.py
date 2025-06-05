2c5d4a3a89be57b86db3f732353dd81935e42cc4
Alejandro <10653@holbertonstudents.com>
2025-06-05 17:22:56 -0300

    9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module for Student class
+"""
+
+
+class Student:
+    """
+    Class that defines a student.
+    
+    Attributes:
+        first_name (str): The first name of the student
+        last_name (str): The last name of the student
+        age (int): The age of the student
+    """
+    
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieve a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing all attributes of the instance
+        """
+        return self.__dict__
