ee9a73be3f792ff14a658d8969cd504396226ea3
Alejandro <10653@holbertonstudents.com>
2025-06-24 18:49:34 +0000

    4-first_table.sql

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Script to create a table called first_table in the current database
+-- Table structure: id INT, name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
