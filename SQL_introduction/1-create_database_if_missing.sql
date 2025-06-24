69b8630fb86ea79b7ab22f76e8469586bd1544ef
Alejandro <10653@holbertonstudents.com>
2025-06-24 18:28:57 +0000

    1-create_database_if_missing.sql

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,3 @@
-CREATE DATABASE hbtn_0c_0;
+-- Script to create the database hbtn_0c_0 if it doesn't exist
+-- This script will not fail if the database already exists
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
