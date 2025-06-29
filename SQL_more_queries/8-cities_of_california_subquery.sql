c174d7eef5ef77fb4faae5c11f6386a6940be01b
Alejandro <10653@holbertonstudents.com>
2025-06-29 17:41:02 +0000

    8-cities_of_california_subquery.sql

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- List all the cities of California, sorted by cities.id, without using JOIN
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'Califormia');
+ORDER BY id ASC;
