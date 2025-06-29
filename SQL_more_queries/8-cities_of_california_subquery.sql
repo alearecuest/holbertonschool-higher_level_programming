3d00f862ed6071bbd8430679f11351358390a892
Alejandro <10653@holbertonstudents.com>
2025-06-29 17:54:53 +0000

    Update error

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- List all the cities of California, sorted by cities.id, without using JOIN
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'Califormia');
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
