e1e966c4c8b990ea8094274008f7988b3de6ab96
Alejandro <10653@holbertonstudents.com>
2025-07-03 16:37:56 -0300

    Update pycodestyle

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,15 +12,15 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     first_state = session.query(State).order_by(State.id).first()
-    
+
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
-    
+
     session.close()
