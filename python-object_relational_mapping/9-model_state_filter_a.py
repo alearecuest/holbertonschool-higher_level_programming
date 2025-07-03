3e588fa64f22b231d18e8a032310c7afbad4997f
Alejandro <10653@holbertonstudents.com>
2025-07-03 16:39:59 -0300

    Update pycodestyle

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     states_with_a = session.query(State).filter(
         State.name.like('%a%')).order_by(State.id).all()
-    
+
     for state in states_with_a:
         print("{}: {}".format(state.id, state.name))
-    
+
     session.close()
